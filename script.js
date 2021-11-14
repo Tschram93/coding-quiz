c57210fc50211d58e6d01c9d0b3bc6c040c7dc7d
TS93 <tschram93@yahoo.com>
2021-11-14 18:59:17 -0500

    styling QuizBox

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Query Selectors
+const nextBtn = document.getElementById('nextBtn');
 const restartBtn = document.getElementById('restartBtn');
 const startBtn = document.getElementById('startBtn');
 const exitBtn = document.getElementById('exitBtn');
