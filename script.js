72fc6938f03ebe3e09a70e2dc20bb01765b01a0a
TS93 <tschram93@yahoo.com>
2021-11-14 20:39:55 -0500

    Questions section of QuizBox styled and formatted

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Query Selectors
+const exitBtn = document.getElementById('exitBtn');
 const nextBtn = document.getElementById('nextBtn');
 const restartBtn = document.getElementById('restartBtn');
 const startBtn = document.getElementById('startBtn');
-const exitBtn = document.getElementById('exitBtn');
+const timer = document.getElementById('timer');
