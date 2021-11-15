25eec5d77ba6f9572c52bdba55af4bbb4c92bd63
TS93 <tschram93@yahoo.com>
2021-11-15 00:50:51 -0500

    created begin & return buttons to begin quiz on Rules card; selectors for them created as well

--- src/script.js
+++ src/script.js
@@ -1,34 +1,42 @@
-// Query Selectors
+                // Query Selectors
 
-// Rules Section/Card
-const cardRules = document.querySelector('#cardRules');
+                // Rules Section/Card
+                const cardRules = document.querySelector('#cardRules');
 
-// Question Box
-const options = document.querySelectorAll('.option');
-const quizBox = document.querySelector('#quizBox');
-const questionText = document.querySelector('#questionText');
-const timer = document.querySelector('#timer');
-const timerBox = document.querySelector('#timerBox');
-const totalQuestions = document.querySelector('#totalQuestions');
+                // Question Box
+                const options = document.querySelectorAll('.option');
+                const quizBox = document.querySelector('#quizBox');
+                const questionText = document.querySelector('#questionText');
+                const timer = document.querySelector('#timer');
+                const timerBox = document.querySelector('#timerBox');
+                const totalQuestions = document.querySelector('#totalQuestions');
 
-// Result Box
-const resultBox = document.querySelector('#resultBox');
-const finishedQuiz = document.querySelector('#finishedQuiz');
-const score = document.querySelector('#score');
+                // Result Box
+                const resultBox = document.querySelector('#resultBox');
+                const finishedQuiz = document.querySelector('#finishedQuiz');
+                const score = document.querySelector('#score');
 
-// Buttons
-const exitBtn = document.getElementById('exitBtn'); //Result Box
-const nextBtn = document.getElementById('nextBtn'); // Quiz section
-const restartBtn = document.getElementById('restartBtn'); //Result Box
-const startBtn = document.getElementById('startBtn'); // Rules Section
-
-// Event Listeners
-startBtn.onclick = () => {
-    card
-}
-nextBtn.addEventListener('click', )
-restartBtn.addEventListener('click', )
-exitBtn.addEventListener('click', )
+                // Buttons
+                const beginBtn = document.getElementById('beginBtn'); // Rules Section
+                const exitBtn = document.getElementById('exitBtn'); //Result Box
+                const nextBtn = document.getElementById('nextBtn'); // Quiz section
+                const restartBtn = document.getElementById('restartBtn'); //Result Box
+                const returnBtn = document.getElementById('returnBtn');
+                const startBtn = document.querySelector('#startBtn'); // Blank Screen (on page start up)
+                console.log(startBtn);
 
 
-// Functions
+                // Event Listeners
+                // Start button displays the Rule Card
+                startBtn.addEventListener('click', () => {
+                    cardRules.style.display = "block";
+                })
+                // nextBtn.addEventListener('click', )
+                // restartBtn.addEventListener('click', )
+
+                // exitBtn.addEventListener('click', () => {
+                //     cardRules.style.display = "none";;
+                // })
+
+
+                // Functions
