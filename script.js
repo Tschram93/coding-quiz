cc0a448be98da48f17fd465d306def27ea438909
TS93 <tschram93@yahoo.com>
2021-11-15 00:05:05 -0500

    Selectors added connecting javascript to html elements/sections; onclick started for start button

--- script.js
+++
@@ -1,9 +0,0 @@
-// Query Selectors
-const exitBtn = document.getElementById('exitBtn');
-const nextBtn = document.getElementById('nextBtn');
-const restartBtn = document.getElementById('restartBtn');
-const startBtn = document.getElementById('startBtn');
-const timer = document.getElementById('timer');
-
-
-// 37:30
---
+++ src/script.js
@@ -0,0 +1,34 @@
+// Query Selectors
+
+// Rules Section/Card
+const cardRules = document.querySelector('#cardRules');
+
+// Question Box
+const options = document.querySelectorAll('.option');
+const quizBox = document.querySelector('#quizBox');
+const questionText = document.querySelector('#questionText');
+const timer = document.querySelector('#timer');
+const timerBox = document.querySelector('#timerBox');
+const totalQuestions = document.querySelector('#totalQuestions');
+
+// Result Box
+const resultBox = document.querySelector('#resultBox');
+const finishedQuiz = document.querySelector('#finishedQuiz');
+const score = document.querySelector('#score');
+
+// Buttons
+const exitBtn = document.getElementById('exitBtn'); //Result Box
+const nextBtn = document.getElementById('nextBtn'); // Quiz section
+const restartBtn = document.getElementById('restartBtn'); //Result Box
+const startBtn = document.getElementById('startBtn'); // Rules Section
+
+// Event Listeners
+startBtn.onclick = () => {
+    card
+}
+nextBtn.addEventListener('click', )
+restartBtn.addEventListener('click', )
+exitBtn.addEventListener('click', )
+
+
+// Functions
